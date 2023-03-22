7ae8a746f238cc5bce0ef8443096918eb4f8abb4
perezkariuki <perezkariuki@gmail.com>
2023-03-22 22:24:51 +0300

    new changes Django admin

--- src/personal/models.py
+++ src/personal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+PRIORITY = [
+    ("H","High"),
+    ("M", "Medium"),
+    ("L", "Low")
+]
+
+class Question(model.models):
+    title                   = models.charField(max_length=60)
+    question                = Models.TextField(max_length=400)
+    priority                = models.CharField(max_length=1, choices=PRIORITY)
+
+    def __str__(self):
+        return self.title
--- src/personal/views.py
+++ src/personal/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screen_view(request):
-    print(request.headers)
-    return render(request, "personal/home.html",{})
+
+    context = {}
+    list_of_values =[]
+    list_of_values.append("First Entery")
+    list_of_values.append("Second Entery")
+    list_of_values.append("Third Entery")
+    list_of_values.append("Fourth Entery")
+    context['list_of_values'] = list_of_values
+
+    return render(request, "personal/home.html", context)
