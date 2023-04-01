b5a034e5393e725507443648a4174a11201b66d4
perezkariuki <perezkariuki@gmail.com>
2023-04-01 16:08:17 +0300

    introduction to mode,view,home,base,admin

--- src/personal/models.py
+++ src/personal/models.py
@@ -8,10 +8,14 @@ PRIORITY = [
     ("L", "Low")
 ]
 
-class Question(model.models):
-    title                   = models.charField(max_length=60)
-    question                = Models.TextField(max_length=400)
+class Question(models.Model):
+    title                   = models.CharField(max_length=60)
+    question                = models.TextField(max_length=400)
     priority                = models.CharField(max_length=1, choices=PRIORITY)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "The Question"
+        verbose_name_plural = "Peoples Question"
--- src/personal/views.py
+++ src/personal/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from personal.models import Question
 
 # Create your views here.
 
 def home_screen_view(request):
 
     context = {}
-    list_of_values =[]
-    list_of_values.append("First Entery")
-    list_of_values.append("Second Entery")
-    list_of_values.append("Third Entery")
-    list_of_values.append("Fourth Entery")
-    context['list_of_values'] = list_of_values
+    # list_of_values =[]
+    # list_of_values.append("First Entery")
+    # list_of_values.append("Second Entery")
+    # list_of_values.append("Third Entery")
+    # list_of_values.append("Fourth Entery")
+    # context['list_of_values'] = list_of_values
+
+    questions = Question.objects.all()
+    context['questions'] = questions
 
     return render(request, "personal/home.html", context)
