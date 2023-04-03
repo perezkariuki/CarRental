a319f3ef29e3db90ec66662b3342ec41a700f9c1
perezkariuki <perezkariuki@gmail.com>
2023-04-03 23:06:01 +0300

    display emails on home page

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
-from personal.models import Question
+from account.models import Account
 
 # Create your views here.
 
 def home_screen_view(request):
 
     context = {}
-    # list_of_values =[]
-    # list_of_values.append("First Entery")
-    # list_of_values.append("Second Entery")
-    # list_of_values.append("Third Entery")
-    # list_of_values.append("Fourth Entery")
-    # context['list_of_values'] = list_of_values
 
-    questions = Question.objects.all()
-    context['questions'] = questions
+    accounts = Account.objects.all()
+    context['accounts'] = accounts
 
     return render(request, "personal/home.html", context)
